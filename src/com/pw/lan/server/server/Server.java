d94f3069a53e745fc98c1ddce4e9332311ca3fcb
aradej006 <aradej006@gmail.com>
2016-05-16 21:20:54 +0200

    TLS added (SSLServerSocket and SSLSocket)

--- src/com/pw/lan/server/server/Server.java
+++ src/com/pw/lan/server/server/Server.java
@@ -1,11 +1,12 @@
 package com.pw.lan.server.server;
 
-
+import javax.net.ssl.SSLServerSocketFactory;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
 import java.util.Vector;
+import java.util.concurrent.SynchronousQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,6 +22,11 @@ public class Server implements Runnable {
     private int _lastID = -1;
 
     public Server(int port) {
+        System.setProperty("javax.net.ssl.keyStore", "keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
+//        System.setProperty("java.protocol.handler.pkgs","com.sun.net.ssl.internal.www.protocol");
+//        System.setProperty("javax.net.debug","ssl");
+
         LOGGER.info("Server: Starting...");
         setInetAddress();
         LOGGER.log(Level.INFO, "Server: Setting port={0}", port);
@@ -66,7 +72,7 @@ public class Server implements Runnable {
         }
     }
 
-    synchronized void addClientService(Service clientService) throws IOException {
+    private synchronized void addClientService(Service clientService) throws IOException {
         clientService.init();
         clients.addElement(clientService);
         new Thread(clientService).start();
@@ -80,12 +86,12 @@ public class Server implements Runnable {
         LOGGER.log(Level.INFO,"Server: Removed service {0}.", clientService.getIds());
     }
 
-    synchronized void send(String msg) {
+    private synchronized void send(String msg) {
         for (Service s : clients)
             s.send(msg);
     }
 
-    synchronized int nextID() {
+    private synchronized int nextID() {
         return ++_lastID;
     }
 
@@ -107,7 +113,8 @@ public class Server implements Runnable {
     private boolean setServer() {
         try {
             LOGGER.log(Level.INFO, "Server: Creating ServerSocket with {0}:{1}", new Object[]{inetAddress.getHostAddress(), port});
-            serverSocket = new ServerSocket(port, 10, inetAddress);
+//            serverSocket = new ServerSocket(port, 10, inetAddress);
+            serverSocket = SSLServerSocketFactory.getDefault().createServerSocket(port,10,inetAddress);
             LOGGER.log(Level.INFO, "Server: Started at {0}:{1}", new Object[]{inetAddress.getHostAddress(), port});
             return true;
         } catch (IOException e) {
