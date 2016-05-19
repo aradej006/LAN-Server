85ef10904dd469e5ffb8ea729cdfbf7e83d3ff9c
aradej006 <aradej006@gmail.com>
2016-05-19 14:40:44 +0200

    deleted ftpclient class

--- src/com/pw/lan/server/tftp/TFTPUtils.java
+++ src/com/pw/lan/server/tftp/TFTPUtils.java
@@ -12,22 +12,16 @@ import com.pw.lan.server.tftpserver.TFTPServerClient;
 public class TFTPUtils {
 
 	public static void puts(String s) {
-	 
 		System.out.println(s);
-		
 	}
 	
 	public static void fatalError(String msg) {
-		
 		System.err.println("Fatal error: " + msg);
 		System.exit(1);
-		
 	}
 	
 	public static void clientMessage(TFTPServerClient client, String message) {
-		
 		System.out.println("[" + client.socket.getInetAddress().getHostAddress() + "] " + message);
-		
 	}
 	
 }
